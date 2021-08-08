a3dcf8b9b96b559ad6821d2d1d0cf067b59f92ff
danilo46 <martin046052@gmail.com>
2021-08-08 18:27:38 -0300

    eliminacion inscripto

--- src/main/java/com/scrum/ude/dao/IInscriptoDAO.java
+++
@@ -1,9 +0,0 @@
-package com.scrum.ude.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.scrum.ude.entity.Inscripto;
-
-public interface IInscriptoDAO extends CrudRepository<Inscripto, Long> {
-
-}
