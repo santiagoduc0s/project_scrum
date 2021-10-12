2188c7749072f2d5262ec9dc70f9431e00025e4b
danilo46 <martin046052@gmail.com>
2021-10-12 17:28:34 -0300

    Cada vez que se ve una pagina genera una registro en la tabla paginaUsers

--- src/main/java/com/scrum/ude/controller/PaginaController.java
+++ src/main/java/com/scrum/ude/controller/PaginaController.java
@@ -1,5 +1,8 @@
 package com.scrum.ude.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,17 +11,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.scrum.ude.dao.IUsuarioDAO;
 import com.scrum.ude.entity.Pagina;
+import com.scrum.ude.entity.Usuario;
 import com.scrum.ude.service.PaginaServiceImpl;
+import com.scrum.ude.service.UsuarioServiceImpl;
 
 @Controller
 public class PaginaController {
+	@Autowired
+	private IUsuarioDAO usuarioDAO;
 
     @Autowired
     private UsuarioController usuarioController;
 
     @Autowired
     public PaginaServiceImpl paginaImpl;
+    
+    @Autowired
+    private UsuarioServiceImpl usuarioService;
 
 
     @GetMapping("/verPag/{id}")
@@ -33,6 +44,26 @@ public class PaginaController {
 
         Pagina pagina = null;
         pagina = paginaImpl.obtenerContenido(id);
+        if(pagina!=null) {
+        	
+        	 //usuarioDAO.sa
+        Usuario user=usuarioService.findOne(auth.getName());
+        
+        List<Pagina> paginas= new ArrayList<>();
+        paginas.addAll(user.getPaginas());
+        if(!paginas.contains(pagina)) {
+        	
+            paginas.add(pagina);
+
+        }
+        	
+        System.out.println(user);
+        	
+        user.setPaginas(paginas);
+
+        usuarioDAO.save(user);
+        	
+        }
 
         model.addAttribute("pagina", pagina);
 
